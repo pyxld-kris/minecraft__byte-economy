9a7d90915923f5c188a908db1446a317011234b1
Maximilian Kling <an@maximilian-kling.de>
2021-05-07 20:11:04 +0200

    Implement new Economy Interface of the DevLaunchersLibrary
    Bumps Version to 0.0.4

--- src/main/java/devlaunchers/byteeconomy/commands/GiveByteCommand.java
+++ src/main/java/devlaunchers/byteeconomy/commands/GiveByteCommand.java
@@ -1,6 +1,7 @@
 package devlaunchers.byteeconomy.commands;
 
 import devlaunchers.byteeconomy.ByteEconomy;
+import devlaunchers.economy.Economy;
 import devlaunchers.items.DevLauncherItem;
 import devlaunchers.items.ItemRepository;
 
@@ -18,18 +19,11 @@ public class GiveByteCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            // Create a new ItemStack (type: diamond)
-            ItemStack byteItem = ItemRepository.getItem(DevLauncherItem.ECONOMY_BYTE_ITEM).clone();
-
             int numBytes = 1;
-            if (args[0] != null)
+            if (args.length > 0 && args[0] != null)
                 numBytes = Integer.parseInt(args[0]);
 
-            // Set the amount of the ItemStack
-            byteItem.setAmount(numBytes);
-
-            // Give the player our items (comma-seperated list of all ItemStack)
-            player.getInventory().addItem(byteItem);
+            Economy.getInstance().giveMoney(player, numBytes);
 
             System.out.println("[ByteEconomy] [LOG] Gave byte$! Amount: " + numBytes);
         }
